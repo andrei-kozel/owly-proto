d270a2b8e702acac851d625664342a8046e20d66
Andrei Kozel <kozel.andrei.94@gmail.com>
2024-10-20 15:25:05 +0200

    experimental

--- golang/go.mod
+++ golang/go.mod
@@ -1,11 +1,10 @@
-module github.com/andrei-kozel/owly-proto
+module github.com/andrei-kozel/owly-proto/golang
 
-go 1.23.0
+go 1.23.2
 
 require (
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.35.1
-	github.com/andrei-kozel/owly-proto v0.0.10
 )
 
 require (
--- run.sh
+++ run.sh
@@ -1,72 +1,67 @@
 set -e # Exit on any error
 
-SERVICES=$1 # Accept a list of services as a parameter (comma-separated string)
+SERVICE_NAME=$1
 RELEASE_VERSION=$2
 
 # Debug: Initial parameters
 echo "::group::Debug: Parameters"
-echo "SERVICES: $SERVICES"
+echo "SERVICE_NAME: $SERVICE_NAME"
 echo "RELEASE_VERSION: $RELEASE_VERSION"
 echo "::endgroup::"
 
 # Install necessary dependencies
 echo "::group::Installing dependencies"
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+echo "Installed protobuf-compiler and golang-goprotobuf-dev"
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-echo "Installed dependencies"
+echo "Installed protoc-gen-go and protoc-gen-go-grpc"
 echo "::endgroup::"
 
-# Split the services string into an array (assuming it's comma-separated)
-IFS=',' read -r -a SERVICE_ARRAY <<< "$SERVICES"
+# Compile the .proto files
+echo "::group::Compiling .proto files"
 
-for SERVICE_NAME in "${SERVICE_ARRAY[@]}"; do
-    # Compile the .proto files for each service
-    echo "::group::Compiling .proto files for ${SERVICE_NAME}"
+# Check if folder exists
+# If not, create it
+if [ ! -d "golang" ]; then
+    mkdir golang
+fi
 
-    if [ ! -d "golang" ]; then
-        mkdir golang
-    fi
+protoc --go_out=./golang --go_opt=paths=source_relative \
+    --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
+    ./${SERVICE_NAME}/*.proto
+echo "Compiled proto files for service: ${SERVICE_NAME}"
+echo "::endgroup::"
 
-    protoc --go_out=./golang --go_opt=paths=source_relative \
-        --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
-        ./${SERVICE_NAME}/*.proto
-    echo "Compiled proto files for service: ${SERVICE_NAME}"
-    echo "::endgroup::"
+# Ensure the directory exists
+echo "::group::Checking service directory"
+if [ ! -d "golang/${SERVICE_NAME}" ]; then
+    echo "::error::Directory golang/${SERVICE_NAME} does not exist"
+    exit 1
+fi
+echo "Service directory exists: golang/${SERVICE_NAME}"
+echo "::endgroup::"
 
-    # Ensure the service directory exists
-    echo "::group::Checking service directory for ${SERVICE_NAME}"
-    if [ ! -d "golang/${SERVICE_NAME}" ]; then
-        echo "::error::Directory golang/${SERVICE_NAME} does not exist"
-        exit 1
-    fi
-    echo "Service directory exists: golang/${SERVICE_NAME}"
-    echo "::endgroup::"
+# Initialize the Go module inside the service directory
+echo "::group::Go mod init and tidy"
+cd golang
+echo "Current directory: $(pwd)"
+ls -la
+go mod init github.com/andrei-kozel/owly-proto/golang|| true
+go mod tidy
+echo "Go module initialized and tidied for service: ${SERVICE_NAME}"
+echo "::endgroup::"
 
-    # Initialize and tidy the Go module for each service
-    echo "::group::Go mod init and tidy for ${SERVICE_NAME}"
-    cd golang/${SERVICE_NAME}
-    go mod init github.com/andrei-kozel/owly-proto/golang/${SERVICE_NAME} || true
-    go mod tidy
-    echo "Go module initialized and tidied for ${SERVICE_NAME}"
-    cd ../..
-    echo "::endgroup::"
-
-    # Commit changes
-    echo "::group::Committing changes"
-    git config --global user.email "kozel.andrei.94@gmail.com"
-    git config --global user.name "Andrei Kozel"
-    git add .
-    git commit -am "proto update" || true
-    echo "Committed changes"
-    echo "::endgroup::"
-
-    # Create a single tag for all services
-    echo "::group::Creating single tag"
-    git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} -m "Release ${RELEASE_VERSION}"
-    git push origin --tags
-    echo "Created and pushed tag golang/${SERVICE_NAME}/${RELEASE_VERSION}"
-    echo "::endgroup::"
-done
-
-
+# Commit and push the changes
+echo "::group::Committing and tagging"
+echo "Current directory: $(pwd)"
+cd ..
+echo "Current directory: $(pwd)"
+git config --global user.email "kozel.andrei.94@gmail.com"
+git config --global user.name "Andrei Kozel"
+git add .
+git commit -am "proto update" || true
+git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+echo "Committed and pushed tag ${RELEASE_VERSION}"
+echo "::endgroup::"
